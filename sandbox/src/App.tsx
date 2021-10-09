284aadbcfaa010d7228ac0a9b52c238df1fc57f3
Youssof Fahmy <youssoffahmy@gmail.com>
2021-10-09 17:04:49 -0400

    Updated

---
+++ sandbox/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {FreeButton} from '../../dist'
+
+function App() {
+  return (
+    <div className="App">
+      <FreeButton>BUTTON!</FreeButton>
+    </div>
+  );
+}
+
+export default App;
